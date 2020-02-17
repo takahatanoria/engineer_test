8c7867272141f9cafe2f07a46fcbc201bdfb582a
takahata-noria <takahatanoria@gmail.com>
2020-02-18 02:38:43 +0900

    閲覧機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'boards#index'
-  get 'boards' => 'boards#index'
-  get 'boards/new'  =>  'boards#new' 
-  post 'boards'      =>  'boards#create' 
+  resources :boards do
+    resources :comments, only: [:create, :destroy]
+  end
 
 end
